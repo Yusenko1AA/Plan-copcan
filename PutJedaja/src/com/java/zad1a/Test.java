21024c7f87e4c56ecdef9f8c018c5d8d2e7526bc
Alexander Yusenko <ainzualgone@gmail.com>
2022-06-01 18:34:33 +0300

    1 коммит он всех ебит) гы

---
+++ PutJedaja/src/com/java/zad1a/Test.java
@@ -0,0 +1,8 @@
+package com.java.zad1a;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Пошла жара");
+        System.out.println("Я Стану Программистом");
+    }
+}

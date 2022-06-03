e2cdacc0369fcc1618c6a22d40695a84e4985fed
Alexander Yusenko <ainzualgone@gmail.com>
2022-06-03 13:16:24 +0300

    1 задание из 3 пака квестов)

--- PutJedaja/src/com/java/zad1a/Test.java
+++
@@ -1,8 +0,0 @@
-package com.java.zad1a;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Пошла жара");
-        System.out.println("Я Стану Программистом");
-    }
-}

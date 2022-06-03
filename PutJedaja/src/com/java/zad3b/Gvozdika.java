4d51591d751ebe6695b36707cdf2e92a51788435
Alexander Yusenko <ainzualgone@gmail.com>
2022-06-03 13:59:42 +0300

    2 задание из 3 пака квестов)

---
+++ PutJedaja/src/com/java/zad3b/Gvozdika.java
@@ -0,0 +1,10 @@
+package com.java.zad3b;
+
+public class Gvozdika extends Flower {
+    private final static String name = "Гвоздика";
+
+    public Gvozdika(double price, Color color) {
+        super(price, name, color);
+    }
+}
+
---
+++ PutJedaja/src/com/java/zad3b/Tulip.java
@@ -0,0 +1,9 @@
+package com.java.zad3b;
+
+public class Tulip extends Flower {
+    private final static String name = "Тюльпан";
+
+    public Tulip(double price, Color color) {
+        super(price, name, color);
+    }
+}
